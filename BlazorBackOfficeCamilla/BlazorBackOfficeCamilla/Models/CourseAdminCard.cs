b4a03efc37cc54c808cbcc81437f2e250ded143a
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-05-24 12:39:52 +0200

    Add project files.

---
+++ BlazorBackOfficeCamilla/BlazorBackOfficeCamilla/Models/CourseAdminCard.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorBackOfficeCamilla.Models;
+
+public class CourseAdminCard
+{
+    public string? Id { get; set; }
+    public string? ImageUri { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public decimal? Price { get; set; }
+    public decimal? DiscountPrice { get; set; }
+
+}
---
+++ BlazorBackOfficeCamilla/BlazorBackOfficeCamilla/Models/CourseCreate.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorBackOfficeCamilla.Models;
+
+public class CourseCreate
+{
+    public string? Id { get; set; }
+    public string? Title { get; set; }
+}
