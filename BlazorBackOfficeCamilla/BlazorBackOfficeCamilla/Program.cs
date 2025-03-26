0dadce8750680d70438faa15cdff6b272d675713
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2025-03-26 21:47:25 +0100

    Update .gitignore and remove appsettings.json
    Updated .gitignore to ignore FodyWeavers.xsd and appsettings.json in the BlazorBackOfficeCamilla directory. Removed appsettings.json, which contained configuration settings like connection strings, logging levels, allowed hosts, and a GraphQL endpoint URL.

--- BlazorBackOfficeCamilla/BlazorBackOfficeCamilla/Program.cs
+++ BlazorBackOfficeCamilla/BlazorBackOfficeCamilla/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddAuthorization(x =>
 });
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-builder.Services.AddSingleton(s => new GraphQLHttpClient("https://courseprovider-silicon-camilla.azurewebsites.net/api/graphql?code=LL18Cb5IbCut7M9hw0bCm5RFI7hOjOJwHOmU6-Dyd1TXAzFuSGQ4FA%3D%3D", new SystemTextJsonSerializer()));
+builder.Services.AddSingleton(s => new GraphQLHttpClient(Environment.GetEnvironmentVariable("GraphQL_Courses"), new SystemTextJsonSerializer()));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
